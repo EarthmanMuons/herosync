e18289fcce860e35c83452bce719cd351d13488a
Aaron Bull Schaefer <aaron@elasticdog.com>
2025-03-04 15:07:32 -0800

    fix: show help output for root command
    Rather than have it implicitly run the `status` subcommand, this
    behavior will be more intuitive. It was also problematic, as there was
    no way to pass through any local flags to the status command, which
    was problematic.

--- cmd/root.go
+++ cmd/root.go
@@ -15,14 +15,12 @@ var (
 	cameraIP string
 )
 
-// The bare root command runs status by default.
 var rootCmd = &cobra.Command{
 	Use:   "herosync",
 	Short: "Download, combine, and publish GoPro videos with ease",
 	Long: `A tool for automating GoPro video transfers. Download media files over WiFi,
 combine chapters into complete videos, clean up storage, and optionally publish
 to YouTube.`,
-	RunE: runStatus,
 }
 
 func Execute() {
